2196a38fa446386eac4582965f5a62695db3592b
MathewZhuravel <mathew.juravel@gmail.com>
2013-08-24 15:39:56 +0400

    fixed web.js: added use static files

--- web.js
+++ web.js
@@ -7,9 +7,13 @@ var buf = new Buffer(10);
 buf = fs.readFileSync("index.html");
 
 app.get('/', function(request, response) {
-  console.log("Fuck! Trying to return " + request);
   response.send(buf.toString());
 });
+
+app.use(express.static(__dirname + '/assets'));
+app.use(express.static(__dirname + '/assets/css'));
+app.use(express.static(__dirname + '/assets/js'));
+app.use(express.static(__dirname + '/assets/img'));
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
