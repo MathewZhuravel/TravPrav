4953621e287ab2c5b9028feb16c0e7b985d49913
MathewZhuravel <mathew.juravel@gmail.com>
2013-08-24 15:48:20 +0400

    fix2

--- web.js
+++ web.js
@@ -10,10 +10,10 @@ app.get('/', function(request, response) {
   response.send(buf.toString());
 });
 
-app.use(express.static(__dirname + '/assets'));
-app.use(express.static(__dirname + '/assets/css'));
-app.use(express.static(__dirname + '/assets/js'));
-app.use(express.static(__dirname + '/assets/img'));
+app.use('/assets',     express.static(__dirname + '/assets'));
+app.use('/assets/css', express.static(__dirname + '/assets/css'));
+app.use('/assets/js',  express.static(__dirname + '/assets/js'));
+app.use('/assets/img', express.static(__dirname + '/assets/img'));
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
